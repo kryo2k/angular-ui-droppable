16dbb23aa9052417541128117ca37fc778b3a456
Hans Doller <kryo2k@gmail.com>
2014-01-31 00:38:18 -0600

    injecting angular ng-model into ui.helper if found.

--- src/droppable.js
+++ src/droppable.js
@@ -1,49 +1,132 @@
 'use strict';
 
-angular.module('ui.droppable', [])
+(function(angular){
 
-  /*
-   jQuery UI Draggable plugin wrapper
+  function combineCallbacks(first,second){
+    if( second && (typeof second === 'function') ){
+      return function(e,ui){
+        first(e,ui);
+        second(e,ui);
+      };
+    }
+    return first;
+  }
 
-   @param [ui-draggable] {object} Options to pass to $.fn.draggable() merged onto ui.config
-  */
-  .value('uiDraggableConfig',{})
-  .directive('uiDraggable', [
-    'uiDraggableConfig',
-    '$log',
-    function (uiDraggableConfig, log) {
-      return {
-        link: function(scope, element, attrs) {
-          element.draggable(uiDraggableConfig);
-          scope.$watch(attrs.uiDraggable, function(newVal){
-            angular.forEach(newVal, function(value, key){
-              element.draggable('option', key, value);
+  function InjectAngularProp(props, callback) {
+    return function (evt, ui) {
+      if(!ui.helper) {
+        ui.helper = {};
+      }
+
+      ui.helper.dragdrop = props;
+
+      (callback||function(){})(evt, ui, props);
+    };
+  }
+
+  angular.module('ui.droppable', [])
+
+    /*
+     jQuery UI Draggable plugin wrapper
+
+     @param [ui-draggable] {object} Options to pass to $.fn.draggable() merged onto ui.config
+    */
+    .value('uiDraggableConfig',{})
+    .directive('uiDraggable', [
+      'uiDraggableConfig',
+      '$log',
+      function (uiDraggableConfig, log) {
+        return {
+          require: '?ngModel',
+          link: function(scope, element, attrs, ngModel) {
+            var
+            injectProps = {
+              element: element
+            },
+            events = {
+              create: new InjectAngularProp(injectProps),
+              drag:   new InjectAngularProp(injectProps),
+              start:  new InjectAngularProp(injectProps),
+              stop:   new InjectAngularProp(injectProps)
+            };
+
+            if (ngModel) {
+              ngModel.$render = function() {
+                injectProps.model = ngModel.$modelValue;
+              };
+            }
+
+            angular.extend(opts, uiDraggableConfig);
+            angular.forEach(events, function(val, key) {
+              opts[key] = combineCallbacks(val, opts[key]);
             });
-          }, true);
+
+            element.draggable(opts);
+
+            scope.$watch(attrs.uiDraggable, function(newVal){
+              angular.forEach(newVal, function(value, key){
+                if(events[key]) {
+                  value = combineCallbacks(events[key], value);
+                }
+                element.draggable('option', key, value);
+              });
+            }, true);
+          }
         }
       }
-    }
-  ])
+    ])
 
-  /*
-   jQuery UI Droppable plugin wrapper
+    /*
+     jQuery UI Droppable plugin wrapper
 
-   @param [ui-droppable] {object} Options to pass to $.fn.droppable() merged onto ui.config
-  */
-  .value('uiDroppableConfig',{})
-  .directive('uiDroppable', [
-    'uiDroppableConfig',
-    '$log',
-    function (uiDroppableConfig, log) {
-      return {
-        link: function(scope, element, attrs) {
-          element.droppable(uiDroppableConfig);
-          scope.$watch(attrs.uiDroppable, function(newVal){
-            angular.forEach(newVal, function(value, key) {
-              element.droppable('option', key, value);
+     @param [ui-droppable] {object} Options to pass to $.fn.droppable() merged onto ui.config
+    */
+    .value('uiDroppableConfig',{})
+    .directive('uiDroppable', [
+      'uiDroppableConfig',
+      '$log',
+      function (uiDroppableConfig, log) {
+        return {
+          require: '?ngModel',
+          link: function(scope, element, attrs, ngModel) {
+            var
+            opts = {},
+            injectProps = {
+              element: element
+            },
+            events = {
+              activate:   new InjectAngularProp(injectProps),
+              create:     new InjectAngularProp(injectProps),
+              deactivate: new InjectAngularProp(injectProps),
+              drop:       new InjectAngularProp(injectProps),
+              out:        new InjectAngularProp(injectProps),
+              over:       new InjectAngularProp(injectProps)
+            };
+
+            if (ngModel) {
+              ngModel.$render = function() {
+                injectProps.model = ngModel.$modelValue;
+              };
+            }
+
+            angular.extend(opts, uiDroppableConfig);
+            angular.forEach(events, function(val, key) {
+              opts[key] = combineCallbacks(val, opts[key]);
             });
-          }, true);
+
+            element.droppable(opts);
+
+            scope.$watch(attrs.uiDroppable, function(newVal){
+              angular.forEach(newVal, function(value, key) {
+                if(events[key]) {
+                  value = combineCallbacks(events[key], value);
+                }
+                element.droppable('option', key, value);
+              });
+            }, true);
+          }
         }
       }
-    }
-  ]);
+    ]);
+
+})(angular);
