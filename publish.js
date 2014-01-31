f853c7eaf1140cc99e6f9c5cd1273255e032ca4e
Hans Doller <kryo2k@gmail.com>
2014-01-30 19:03:40 -0600

    Forgot some files..

--- publish.js
+++ publish.js
@@ -22,9 +22,7 @@ module.exports = function() {
   return {
     humaName : 'UI.Droppable',
     repoName : 'ui-droppable',
-    inlineHTML : fs.readFileSync(__dirname + '/demo/demo.html'),
-    inlineJS : fs.readFileSync(__dirname + '/demo/demo.js'),
-    css: css_dependencies.map(putThemInVendorDir).concat(['demo/demo.css']),
+    css: css_dependencies.map(putThemInVendorDir),
     js : js_dependencies.map(putThemInVendorDir).concat(['dist/droppable.js']),
     tocopy : css_dependencies.concat(js_dependencies)
   };
